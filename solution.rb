288a0c26cd1adc655bed19baefb9f3dc292119ec
danielvebo90 <danielvebo@gmail.com>
2018-09-18 20:44:54 -0500

    cambio final

--- solution.rb
+++ solution.rb
@@ -1,5 +1,9 @@
 require "sinatra"
 
-get '/:nombre' do
-  erb :indexs
+get '/' do
+  unless params[:nombre]
+    "<h1> Hola desconocido! </h1>"
+  else
+    "<h1> Hola #{params[:nombre]}! </h1>"
+  end
 end
